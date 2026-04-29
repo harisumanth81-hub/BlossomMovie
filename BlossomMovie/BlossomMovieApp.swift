552b3e35b5dac6e5782c74a2041e7f0c95018ab5
HariSumanthThalluru <hthtd@umsystem.edu>
2026-04-29 11:33:21 -0500

    Initial Commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Hari Sumanth Thalluru on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
